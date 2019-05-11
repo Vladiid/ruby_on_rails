85fa2fe2f284507a4766f045d77064189f58c8ae
Vladiid <vladiid84@gmail.com>
2019-05-11 21:38:04 +0300

    Create app.rb

---
+++ Projects/MyApp/app.rb
@@ -0,0 +1,11 @@
+puts "Вы довольны своей зарплатой?"
+answer = gets.strip.capitalize
+
+if answer == "Y"
+  puts "Очень хорошо!"
+
+elsif answer == "N"
+puts"Очень плохо!"
+     else
+       puts "Вы ввели не верные значения"
+       end
