983ab79479ee175870f0371d2ef01caa8ed03eea
Vladiid <vladiid84@gmail.com>
2019-05-12 20:31:09 +0300

    Create if_elsif_else.rb

---
+++ Projects/MyApp/if_elsif_else.rb
@@ -0,0 +1,16 @@
+
+number = rand(1..100)
+print"Guess the number? (1..100):"
+a = gets.to_i
+
+if number == a
+  puts "Guessed"
+
+elsif number > a
+puts "Wrong answer, more"
+
+else
+puts"Wrong answer, less"
+end
+
+puts "number = #{number}"
