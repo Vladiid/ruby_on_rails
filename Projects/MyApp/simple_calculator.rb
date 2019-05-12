dfb2dbd51b73fd6574181deca3f7d1caa3f78a4f
Vladiid <vladiid84@gmail.com>
2019-05-12 19:16:00 +0300

    Create simple_calculator.rb

---
+++ Projects/MyApp/simple_calculator.rb
@@ -0,0 +1,28 @@
+print "Enter number A: "
+a = gets.to_f
+print "Enter number B: "
+b = gets.to_f
+print "What do you want to do (+ - * /): "
+symbol = gets.strip
+
+if symbol == "/" && b == 0
+  puts "You can't divide by zero"
+  exit
+end
+
+if symbol == "+"
+  print"#{a} + #{b} = #{a + b}"
+end
+
+if symbol == "-"
+  print "#{a} - #{b} = #{a - b}"
+end
+
+if symbol == "*"
+  print "#{a} * #{b} = #{a * b}"
+end
+
+if symbol == "/"
+  print "#{a} / #{b} = #{a / b}"
+end
+
