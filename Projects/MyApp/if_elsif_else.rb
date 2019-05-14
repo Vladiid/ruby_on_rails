112f6656d6b019cc954bcb8601ae093da7cca0ac
Vladiid <vladiid84@gmail.com>
2019-05-14 20:27:24 +0300

    Update if_elsif_else.rb

--- Projects/MyApp/if_elsif_else.rb
+++ Projects/MyApp/if_elsif_else.rb
@@ -1,16 +1,26 @@
 
 number = rand(1..100)
-print"Guess the number? (1..100):"
+1.upto (10) do |aa|
+  10.downto (1) do |bb|
+  puts "Try #{aa}, Left #{bb}"
+print"Guess the number?  (1..100):"
 a = gets.to_i
 
-if number == a
-  puts "Guessed"
 
+if number == a
+  puts "Guessed!!!"
+exit
 elsif number > a
 puts "Wrong answer, more"
 
-else
+elsif number < a
 puts"Wrong answer, less"
 end
 
 puts "number = #{number}"
+    if aa == 0 or bb == 00
+      puts "Game is over"
+      exit
+    end
+  end
+  end
