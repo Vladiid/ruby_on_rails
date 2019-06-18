31ccb297ea32c5a84d51e8d43b8c156d5ddcb915
Vladiid <vladiid84@gmail.com>
2019-06-18 20:49:08 +0300

    Update arr.rb

--- Projects/MyApp/arr.rb
+++ Projects/MyApp/arr.rb
@@ -1,23 +1,20 @@
-#people = ["alex", "julia", "sam", "marry", "tom", "monika"]
+
 
 people = %w[alex mila vlad tania antony sweet]
 
-s = 'y'
-while s == 'y'
+
+loop do
 
 x = 0
 people.each do |name|
   x = x+1
   puts"#{x}. #{name}"
-
 end
 
-  puts "Who you kill?"
-  n = gets.to_i
-  people.delete_at n-1
+  puts "Who you add?"
+  n = gets.to_s
+  people.push (n)
   puts "#{x} #{n}"
-puts 'Continue?'
-s = gets.strip.downcase
 end
 
 
